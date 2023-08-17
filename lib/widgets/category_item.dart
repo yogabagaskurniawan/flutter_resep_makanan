b6ec01cb1e1e5dd702e44215eeea532693148687
yogabagaskurniawan <yogabag4s@gmail.com>
2023-08-17 13:22:41 +0700

    membuat home page

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import '../layouts/category_page.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imagePath;
+
+  CategoryItem({
+    required this.id,
+    required this.title,
+    required this.imagePath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 7.0,
+        right: 7.0,
+      ),
+      child: Column(
+        children: [
+          Container(
+            height: 70.0,
+            width: 70.0,
+            child: Card(
+              elevation: 6.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0),
+              ),
+              child: InkWell(
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    // mengambail layouts import '../layouts/category_page.dart';
+                    CategoryRecipesPage.routePath,
+                    arguments: {
+                      'id': id,
+                      'title': title,
+                    },
+                  );
+                },
+                borderRadius: BorderRadius.circular(40.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Image.asset(imagePath),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Text(title),
+            margin: EdgeInsets.only(
+              top: 10.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_back_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomBackButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => Navigator.pop(context),
+      child: Container(
+        height: 30.0,
+        width: 30.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50.0),
+          color: Colors.orange,
+          boxShadow: [
+            const BoxShadow(
+              color: Colors.black54,
+              blurRadius: 4,
+            ),
+          ],
+        ),
+        child: const Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
